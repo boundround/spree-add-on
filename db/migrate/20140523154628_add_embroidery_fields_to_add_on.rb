d66664ec06795d9d9217b4e13549ca2e549ee863
David Cook <david@photosbycook.com>
2014-05-23 10:26:56 -0600

    Fix migration

--- db/migrate/20140523154628_add_embroidery_fields_to_add_on.rb
+++ db/migrate/20140523154628_add_embroidery_fields_to_add_on.rb
@@ -1,10 +1,10 @@
 class AddEmbroideryFieldsToAddOn < ActiveRecord::Migration
   def change
-  	add_column :line_item_add_on, :embroidery_text, :string
-  	add_column :line_item_add_on, :embroidery_font, :string
-  	add_column :line_item_add_on, :embroidery_color, :string
-  	add_column :add_on, :show_embroidery, :boolean, :default => false
-  	add_column :add_on, :embroidery_options, :string
+  	add_column :spree_line_item_add_ons, :embroidery_text, :string
+  	add_column :spree_line_item_add_ons, :embroidery_font, :string
+  	add_column :spree_line_item_add_ons, :embroidery_color, :string
+  	add_column :spree_add_ons, :show_embroidery, :boolean, :default => false
+  	add_column :spree_add_ons, :embroidery_options, :string
 
   end
 end
